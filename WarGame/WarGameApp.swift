d8d281fc82f3e80ccd0c2d1a9796ec52ee12d0d3
Joshua Hobson <32719624+joshuahobson@users.noreply.github.com>
2023-05-29 23:51:58 -0500

    Initial commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by Joshua Hobson on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
